e4d59613a6e4078507beb5db975f625d318beea2
sometimesdigital <dominika@sometimes.digital>
2024-10-25 18:46:39 +0200

    update redirect_uri

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import { SCOPES } from "@interfaces/users";
 
 export const config = {
   client_id: "63d690649f2c4b548c9509f8ebda8e7f",
-  redirect_uri: "http://localhost:5173/around-the-world/",
+  redirect_uri: "https://sometimes.digital/around-the-world",
   scope: [
     SCOPES.USER_TOP_READ,
     SCOPES.USER_READ_PRIVATE,
